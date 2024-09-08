0b011adc0b3c798c395a453a9ed3d41a46d852ce
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-08 09:48:43 +0600

    adding client site custom colors setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,30 @@
-import { keepTheme } from "keep-react/keepTheme";
+import { colors, keepTheme } from "keep-react/keepTheme";
 
 const config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    colors: {
+      black: "#000",
+      white: "#fff",
+      orange: "#f26300",
+      blue: "#2453d4",
+      gray: {
+        primary: "#676770",
+        secondary: "#6d758d",
+        light: "#acacac",
+      },
+    },
+  },
 };
 
 export default keepTheme(config);
