123ab06d80dc60ce67d6cdae67d9a190c20b009c
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-09 20:57:28 +0600

    adding server site get single product API

--- server/src/controllers/products.controller.js
+++ server/src/controllers/products.controller.js
@@ -1,6 +1,15 @@
 const asyncHandler = require("../middlewares/asyncHandler");
 const ProductModel = require("../models/product.model");
 /**
+ * @description fetch product by id
+ * @route GET /api/v1/products/:id
+ * @access public
+ */
+const getProduct = asyncHandler(async (req, res) => {
+  const product = await ProductModel.findById(req.params.id);
+  res.json(product);
+});
+/**
  * @description fetch all products
  * @route GET /api/v1/products
  * @access public
@@ -9,4 +18,4 @@ const getProducts = asyncHandler(async (req, res) => {
   const products = await ProductModel.find({});
   res.json(products);
 });
-module.exports = { getProducts };
+module.exports = { getProducts, getProduct };
--- server/src/routes/products.route.js
+++ server/src/routes/products.route.js
@@ -1,7 +1,11 @@
-const { getProducts } = require("../controllers/products.controller");
+const {
+  getProducts,
+  getProduct,
+} = require("../controllers/products.controller");
 
 const router = require("express").Router();
 
+router.route("/:id").get(getProduct);
 router.route("/").get(getProducts);
 
 module.exports = router;
