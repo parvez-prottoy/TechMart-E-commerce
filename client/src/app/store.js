a80c458753d457273379e5ee9b06ccfbd5cc07ac
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-10 19:25:06 +0600

    adding client site add to cart slice

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "../features/product/productSlice";
+import cartReducer from "../features/cart/cartSlice";
 
 const store = configureStore({
   reducer: {
     [productSlice.reducerPath]: productSlice.reducer,
+    cart: cartReducer,
   },
   middleware: (getDefaultMiddlewares) =>
     getDefaultMiddlewares().concat(productSlice.middleware),
---
+++ client/src/features/cart/cartSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [] };
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      // note: total price
+      state.itemsPrice = state.cartItems.reduce(
+        (total, item) => total + item.price * item.qty,
+        0
+      );
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
+});
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
